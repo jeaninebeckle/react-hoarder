2d5445a6bd178333ec80cb74d3b0404ca4c12d44
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-09-01 22:34:24 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import connection from '../helpers/data/connection';
+import Auth from '../components/pages/Auth/Auth';
+import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+
 import './App.scss';
+
+connection();
 
 class App extends React.Component {
   render() {

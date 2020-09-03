88b9fc9802b02bb6cd10039c4ae5113394bc7c1e
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-09-02 20:25:12 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   BrowserRouter,
   Redirect,
@@ -8,20 +8,70 @@ import {
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import connection from '../helpers/data/connection';
-import Auth from '../components/pages/Auth/Auth';
 import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+import Home from '../components/pages/Home/Home';
+import Auth from '../components/pages/Auth/Auth';
+import New from '../components/pages/New/New';
+import Stuff from '../components/pages/Stuff/Stuff';
+import Edit from '../components/pages/Edit/Edit';
+import SingleStuff from '../components/pages/SingleStuff/SingleStuff';
 
 import './App.scss';
 
 connection();
 
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component { ...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">
-        <i className="fab fa-react"></i>I am a button</button>
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavbar authed={authed}/>
+            <div className="container">
+              <Switch>
+                <PrivateRoute path="/home" component={Home} authed={authed}/>
+                <PrivateRoute path="/new" component={New} authed={authed}/>
+                <PrivateRoute path="/stuff" component={Stuff} authed={authed}/>
+                <PrivateRoute path="/edit/:stuffId" component={Edit} authed={authed}/>
+                <PrivateRoute path="/stuff/:stuffId" component={SingleStuff} authed={authed}/>
+                <PublicRoute
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
